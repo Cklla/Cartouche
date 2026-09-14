a86123d0bd5a9f4afab7f9ff5f4a8ebd815f10ae
Cklla <contact@cklla.ch>
2026-09-14 22:39:34 +0200

    chore: versionCode 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         applicationId = "fr.cklla.cartouche"
         minSdk = 24
         targetSdk = 37
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
